6a3581542cc4e1e584d40a8013421342d786d71e
jacksterdealeo <oswaldtechnic@gmail.com>
2025-06-30 16:48:27 -0800

    cleaned mcStitches

--- mcStitches.go
+++ mcStitches.go
@@ -7,13 +7,14 @@ import (
 	"codeberg.org/ostech/craft_to_clonia_textures/stitches"
 )
 
+// Runs all the functions in the "stitches.EveryStitch" array.
 func mcStitches(input_pack_path, output_pack_path string, err_log *strings.Builder) {
-	StitchFuncsToExec := stitches.EveryStitch
+	number_of_stitches := len(stitches.EveryStitch)
+	errors_chan := make(chan error, number_of_stitches)
 
-	errors_chan := make(chan error, len(StitchFuncsToExec))
 	var wg sync.WaitGroup
-	wg.Add(len(StitchFuncsToExec))
-	for _, e := range StitchFuncsToExec {
+	wg.Add(number_of_stitches)
+	for _, e := range stitches.EveryStitch {
 		go func() {
 			if err := e(input_pack_path, output_pack_path); err != nil {
 				errors_chan <- err
@@ -21,8 +22,8 @@ func mcStitches(input_pack_path, output_pack_path string, err_log *strings.Build
 			wg.Done()
 		}()
 	}
-
 	wg.Wait()
+
 	close(errors_chan)
 	for e := range errors_chan {
 		err_log.WriteString(e.Error())
