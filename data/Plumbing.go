1c312925075a0202629f808300dbdb35278306dd
jacksterdealeo <oswaldtechnic@zoho.com>
2026-01-30 03:16:30 -0900

    deleted mcmetaReader.go duplicate and added some more types to the data folder

--- data/Plumbing.go
+++ data/Plumbing.go
@@ -1,9 +1,14 @@
 package data
 
 import (
+	"errors"
 	"image"
+	"image/color"
 	"log"
 	"path/filepath"
+
+	mcmetahandler "codeberg.org/ostech/craft_to_clonia_textures/mcmetaHandler"
+	"github.com/disintegration/imaging"
 )
 
 type SimpleTexture struct {
@@ -28,12 +33,24 @@ type SimpleConversion struct {
 
 // Returns the texture Minecraft inPath appended with texture name.
 func (e *SimpleConversion) ReadPath() string {
-	return filepath.Join(CraftPaths[e.InPath], e.InTexture)
+	return filepath.Join(GetCraftPath(e.InPath), e.InTexture)
 }
 
 // Returns the texture Mineclonia outPath appended with texture name.
 func (e *SimpleConversion) SavePath() string {
-	return filepath.Join(CloniaPaths[e.OutPath], e.OutTexture)
+	return filepath.Join(GetCloniaPath(e.OutPath), e.OutTexture)
+}
+
+func (e *SimpleConversion) Convert(inputPackLocation string, outputPackLocation string) error {
+	err := copyTextureAnimated(
+		filepath.Join(inputPackLocation, GetCraftPath(e.InPath), e.InTexture),
+		filepath.Join(outputPackLocation, GetCloniaPath(e.OutPath), e.OutTexture),
+		e.FramesAllowed,
+	)
+	if err != nil {
+		return errors.New(err.Error() + " ~ " + e.InPath + "::" + e.InTexture)
+	}
+	return nil
 }
 
 type ConversionWithFallback struct {
@@ -87,4 +104,45 @@ func GetCraftPath(shortLocation string) string {
 		log.Panic("Incorrect Location Value!")
 	}
 	return path
+}
+
+func copyTextureAnimated(src string, dest string, framesAllowed int) error {
+	img, err := imaging.Open(src)
+	if err != nil {
+		return err
+	}
+	imgX := img.Bounds().Dx()
+	imgY := img.Bounds().Dy()
+	maxNumOfFrames := imgY / imgX
+	if maxNumOfFrames == 0 { // some 32x31 textures were causing problems.
+		maxNumOfFrames = 1
+	}
+	if framesAllowed < maxNumOfFrames && framesAllowed >= 1 {
+		maxNumOfFrames = framesAllowed
+	}
+	frames, err := mcmetahandler.McmetaReader(src)
+	if err != nil || len(frames) == 0 {
+		for i := range maxNumOfFrames {
+			frames = append(frames, i)
+		}
+	}
+	var outImgNumberOfFrames int
+	if framesAllowed < 1 || framesAllowed > len(frames) {
+		if len(frames) != 0 {
+			outImgNumberOfFrames = len(frames)
+		} else {
+			outImgNumberOfFrames = maxNumOfFrames
+		}
+	} else {
+		outImgNumberOfFrames = framesAllowed
+	}
+	outImg := imaging.New(imgX, imgX*outImgNumberOfFrames, color.NRGBA{0, 0, 0, 0})
+	for i, e := range frames {
+		frame := imaging.Crop(img, image.Rectangle{image.Point{0, e * imgX}, image.Point{imgX, (e * imgX) + imgX}})
+		outImg = imaging.Overlay(outImg, frame, image.Point{0, i * imgX}, 1.0)
+	}
+	if err = imaging.Save(outImg, dest); err != nil {
+		return err
+	}
+	return nil
 }
